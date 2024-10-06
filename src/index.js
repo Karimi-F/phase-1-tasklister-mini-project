ff74dc8a663a4b42785f0c3066f40e3c5f698037
Karimi-F <fridah37karimi@gmail.com>
2024-10-06 22:21:56 +0300

    Builds a To-Do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.querySelector("form").addEventListener("submit",(e) => {
+  e.preventDefault()
+
+   const taskDescription = e.target['new-task-description'].value;
+
+   buildToDo(taskDescription);
+  console.log(e.target['new-task-description'].value);
+  
+})
+function buildToDo(task){
+  const taskItem = document.createElement('li')
+  let p = document.createElement('p')
+  p.textContent = task;
+  let btn = document.createElement('button')
+  btn.addEventListener("click",handleDelete)
+  btn.textContent = 'X';
+  
+  taskItem.appendChild(p);
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector('#tasks').appendChild(taskItem);
+}
+buildToDo('Take out the trash');
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
